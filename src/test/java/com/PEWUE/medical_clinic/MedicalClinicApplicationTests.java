2147616d7564e7fd543602a77715acd0940a3125
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-08-01 21:47:15 +0200

    initial commit

---
+++ src/test/java/com/PEWUE/medical_clinic/MedicalClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PEWUE.medical_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
