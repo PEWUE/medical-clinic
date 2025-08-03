20e19d35db1b31d2b83e8138120f6678e1f1c440
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-08-03 20:05:05 +0200

    PatientService

---
+++ src/main/java/com/PEWUE/medical_clinic/services/PatientService.java
@@ -0,0 +1,50 @@
+package com.PEWUE.medical_clinic.services;
+
+import com.PEWUE.medical_clinic.exceptions.EmailAlreadyExistsException;
+import com.PEWUE.medical_clinic.exceptions.PatientNotFoundException;
+import com.PEWUE.medical_clinic.model.Patient;
+import com.PEWUE.medical_clinic.repositories.PatientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PatientService {
+    private final PatientRepository patientRepository;
+
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
+    public Patient getPatientByEmail(String email) {
+        return patientRepository.findByEmail(email)
+                .orElseThrow(() -> new PatientNotFoundException("Patient with email " + email + " not found"));
+    }
+
+    public void addPatient(Patient patient) {
+        boolean added = patientRepository.add(patient);
+        if (!added) {
+            throw new EmailAlreadyExistsException("Email " + patient.getEmail() + " is already taken");
+        }
+    }
+
+    public void removePatient(String email) {
+        boolean removed = patientRepository.remove(email);
+        if (!removed) {
+            throw new PatientNotFoundException("Patient with email " + email + " does not exist");
+        }
+    }
+
+    public void editPatient(String email, Patient updatedPatient) {
+        boolean edited = patientRepository.edit(email, updatedPatient);
+        if (!edited) {
+            if (patientRepository.findByEmail(email).isEmpty()) {
+                throw new PatientNotFoundException("Patient with email " + email + " does not exist");
+            } else {
+                throw new EmailAlreadyExistsException("Email " + updatedPatient.getEmail() + " is already taken");
+            }
+        }
+    }
+}
