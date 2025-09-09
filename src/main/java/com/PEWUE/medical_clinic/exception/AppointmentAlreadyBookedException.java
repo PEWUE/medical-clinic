3c2d78d290fe69080760413cb4a3d54ef66fd141
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-09-09 11:19:01 +0200

    clean up

--- src/main/java/com/PEWUE/medical_clinic/exception/AppointmentAlreadyBookedException.java
+++ src/main/java/com/PEWUE/medical_clinic/exception/AppointmentAlreadyBookedException.java
@@ -3,6 +3,7 @@ package com.PEWUE.medical_clinic.exception;
 import org.springframework.http.HttpStatus;
 
 public class AppointmentAlreadyBookedException extends MedClinicException {
+
     public AppointmentAlreadyBookedException(String message) {
         super(message, HttpStatus.CONFLICT);
     }
--- src/main/java/com/PEWUE/medical_clinic/exception/AppointmentOverlapException.java
+++ src/main/java/com/PEWUE/medical_clinic/exception/AppointmentOverlapException.java
@@ -3,6 +3,7 @@ package com.PEWUE.medical_clinic.exception;
 import org.springframework.http.HttpStatus;
 
 public class AppointmentOverlapException extends MedClinicException {
+
     public AppointmentOverlapException(String message) {
         super(message, HttpStatus.CONFLICT);
     }
