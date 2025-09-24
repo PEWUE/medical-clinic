2268dbb4e34af43bfdff3a629d0d8fbff5c6e920
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-09-24 18:25:30 +0200

    Institution tests find/add

--- src/main/java/com/PEWUE/medical_clinic/model/Institution.java
+++ src/main/java/com/PEWUE/medical_clinic/model/Institution.java
@@ -11,6 +11,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -24,6 +25,7 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
+@Builder
 @ToString(exclude = {"doctors"})
 public class Institution {
     @Id
---
+++ src/test/java/com/PEWUE/medical_clinic/service/InstitutionServiceTest.java
@@ -0,0 +1,143 @@
+package com.PEWUE.medical_clinic.service;
+
+import com.PEWUE.medical_clinic.model.Doctor;
+import com.PEWUE.medical_clinic.model.Institution;
+import com.PEWUE.medical_clinic.repository.DoctorRepository;
+import com.PEWUE.medical_clinic.repository.InstitutionRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class InstitutionServiceTest {
+    InstitutionRepository institutionRepository;
+    DoctorRepository doctorRepository;
+    InstitutionService institutionService;
+
+    @BeforeEach
+    void setup() {
+        this.institutionRepository = mock(InstitutionRepository.class);
+        this.doctorRepository = mock(DoctorRepository.class);
+        this.institutionService = new InstitutionService(institutionRepository, doctorRepository);
+    }
+
+    @Test
+    void getInstitutions_DataCorrect_InstitutionsReturned() {
+        //given
+        Pageable pageable = PageRequest.of(0, 2);
+        List<Doctor> doctors = List.of(
+                Doctor.builder().id(1L).firstName("name1").lastName("lastname1").specialization("surgeon").build(),
+                Doctor.builder().id(2L).firstName("name2").lastName("lastname2").specialization("gynecologist").build(),
+                Doctor.builder().id(3L).firstName("name3").lastName("lastname3").specialization("dentist").build()
+        );
+        List<Institution> institutions = List.of(
+                Institution.builder()
+                        .id(1L)
+                        .name("Institution 1 name")
+                        .city("City 1")
+                        .postalCode("POSTAL-CODE1")
+                        .street("Street One")
+                        .streetNo("22")
+                        .doctors(List.of(doctors.get(0), doctors.get(1)))
+                        .build(),
+                Institution.builder()
+                        .id(2L)
+                        .name("Institution 2 name")
+                        .city("City 2")
+                        .postalCode("POSTAL-CODE2")
+                        .street("Street Two")
+                        .streetNo("43a")
+                        .doctors(List.of(doctors.get(2)))
+                        .build()
+        );
+
+        Page<Institution> page = new PageImpl<>(institutions, pageable, institutions.size());
+
+        when(institutionRepository.findAll(pageable)).thenReturn(page);
+
+        //when
+        Page<Institution> result = institutionService.find(pageable);
+
+        //then
+        assertAll(
+                () -> assertEquals(2, result.getContent().size()),
+                () -> assertEquals(1L, result.getContent().get(0).getId()),
+                () -> assertEquals("Institution 1 name", result.getContent().get(0).getName()),
+                () -> assertEquals("City 1", result.getContent().get(0).getCity()),
+                () -> assertEquals("POSTAL-CODE1", result.getContent().get(0).getPostalCode()),
+                () -> assertEquals("Street One", result.getContent().get(0).getStreet()),
+                () -> assertEquals("22", result.getContent().get(0).getStreetNo()),
+                () -> assertEquals(2, result.getContent().get(0).getDoctors().size()),
+                () -> assertEquals(1L, result.getContent().get(0).getDoctors().get(0).getId()),
+                () -> assertEquals("name1", result.getContent().get(0).getDoctors().get(0).getFirstName()),
+                () -> assertEquals("lastname1", result.getContent().get(0).getDoctors().get(0).getLastName()),
+                () -> assertEquals("surgeon", result.getContent().get(0).getDoctors().get(0).getSpecialization()),
+                () -> assertEquals(2L, result.getContent().get(0).getDoctors().get(1).getId()),
+                () -> assertEquals("name2", result.getContent().get(0).getDoctors().get(1).getFirstName()),
+                () -> assertEquals("lastname2", result.getContent().get(0).getDoctors().get(1).getLastName()),
+                () -> assertEquals("gynecologist", result.getContent().get(0).getDoctors().get(1).getSpecialization()),
+                () -> assertEquals(2L, result.getContent().get(1).getId()),
+                () -> assertEquals("Institution 2 name", result.getContent().get(1).getName()),
+                () -> assertEquals("City 2", result.getContent().get(1).getCity()),
+                () -> assertEquals("POSTAL-CODE2", result.getContent().get(1).getPostalCode()),
+                () -> assertEquals("Street Two", result.getContent().get(1).getStreet()),
+                () -> assertEquals("43a", result.getContent().get(1).getStreetNo()),
+                () -> assertEquals(1, result.getContent().get(1).getDoctors().size()),
+                () -> assertEquals(3L, result.getContent().get(1).getDoctors().get(0).getId()),
+                () -> assertEquals("name3", result.getContent().get(1).getDoctors().get(0).getFirstName()),
+                () -> assertEquals("lastname3", result.getContent().get(1).getDoctors().get(0).getLastName()),
+                () -> assertEquals("dentist", result.getContent().get(1).getDoctors().get(0).getSpecialization())
+        );
+        verify(institutionRepository).findAll(pageable);
+    }
+
+    @Test
+    void addInstitution_DataCorrect_InstitutionReturned() {
+        //given
+        Institution inputInstitution = Institution.builder()
+                .name("Institution 2 name")
+                .city("City 2")
+                .postalCode("POSTAL-CODE2")
+                .street("Street Two")
+                .streetNo("43a")
+                .doctors(new ArrayList<>())
+                .build();
+
+        Institution institution = Institution.builder()
+                .id(2L)
+                .name("Institution 2 name")
+                .city("City 2")
+                .postalCode("POSTAL-CODE2")
+                .street("Street Two")
+                .streetNo("43a")
+                .doctors(new ArrayList<>())
+                .build();
+
+        when(institutionRepository.save(inputInstitution)).thenReturn(institution);
+
+        //when
+        Institution result = institutionService.add(inputInstitution);
+
+        //then
+        assertAll(
+                () -> assertEquals(2L, result.getId()),
+                () -> assertEquals("Institution 2 name", result.getName()),
+                () -> assertEquals("City 2", result.getCity()),
+                () -> assertEquals("POSTAL-CODE2", result.getPostalCode()),
+                () -> assertEquals("Street Two", result.getStreet()),
+                () -> assertEquals("43a", result.getStreetNo()),
+                () -> assertTrue(result.getDoctors().isEmpty())
+        );
+        verify(institutionRepository).save(inputInstitution);
+    }
+
+
+}
