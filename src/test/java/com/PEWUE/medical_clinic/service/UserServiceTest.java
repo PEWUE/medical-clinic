b3e15e5d652170e03f1d4fe501ca36d47b1aa6e9
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-09-25 00:34:02 +0200

    UserService tests

---
+++ src/test/java/com/PEWUE/medical_clinic/service/UserServiceTest.java
@@ -0,0 +1,117 @@
+package com.PEWUE.medical_clinic.service;
+
+import com.PEWUE.medical_clinic.model.User;
+import com.PEWUE.medical_clinic.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class UserServiceTest {
+    UserRepository userRepository;
+    UserService userService;
+
+    @BeforeEach
+    void setup() {
+        this.userRepository = mock(UserRepository.class);
+        this.userService = new UserService(userRepository);
+    }
+
+    @Test
+    void findUsers_DataCorrect_UsersReturned() {
+        //given
+        Pageable pageable = PageRequest.of(0,2);
+        List<User> users = List.of(
+                User.builder().id(1L).email("email1@user.com").username("username1").password("password1").build(),
+                User.builder().id(2L).email("email2@user.com").username("username2").password("password2").build()
+        );
+        Page<User> page = new PageImpl<>(users, pageable, users.size());
+
+        when(userRepository.findAll(pageable)).thenReturn(page);
+
+        //when
+        Page<User> result = userService.find(pageable);
+
+        //then
+        assertAll(
+                () -> assertEquals(2, result.getContent().size()),
+                () -> assertEquals(1L, result.getContent().get(0).getId()),
+                () -> assertEquals("email1@user.com", result.getContent().get(0).getEmail()),
+                () -> assertEquals("username1", result.getContent().get(0).getUsername()),
+                () -> assertEquals("password1", result.getContent().get(0).getPassword()),
+                () -> assertEquals(2L, result.getContent().get(1).getId()),
+                () -> assertEquals("email2@user.com", result.getContent().get(1).getEmail()),
+                () -> assertEquals("username2", result.getContent().get(1).getUsername()),
+                () -> assertEquals("password2", result.getContent().get(1).getPassword())
+        );
+        verify(userRepository).findAll(pageable);
+    }
+
+    @Test
+    void addUser_DataCorrect_UserReturned() {
+        //given
+        User inputUser = User.builder()
+                .email("email1@user.com")
+                .username("username1")
+                .password("password1")
+                .build();
+        User user = User.builder()
+                .id(1L)
+                .email("email1@user.com")
+                .username("username1")
+                .password("password1")
+                .build();
+
+        when(userRepository.save(inputUser)).thenReturn(user);
+
+        //when
+        User result = userService.add(inputUser);
+
+        //then
+        assertAll(
+                () -> assertEquals(1L, result.getId()),
+                () -> assertEquals("email1@user.com", result.getEmail()),
+                () -> assertEquals("username1", result.getUsername()),
+                () -> assertEquals("password1", result.getPassword())
+        );
+        verify(userRepository).save(inputUser);
+    }
+
+    @Test
+    void changeUserPassword_DataCorrect_UserReturned() {
+        //given
+        Long userId = 3L;
+        String newPassword = "MyNewPa$$word123!";
+        User user = User.builder()
+                .id(3L)
+                .email("email3@user.com")
+                .username("username3")
+                .password("oldPassword")
+                .build();
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        when(userRepository.save(any(User.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        //when
+        User result = userService.changePassword(userId, newPassword);
+
+        //then
+        assertAll(
+                () -> assertEquals(3L, result.getId()),
+                () -> assertEquals("email3@user.com", result.getEmail()),
+                () -> assertEquals("username3", result.getUsername()),
+                () -> assertEquals("MyNewPa$$word123!", result.getPassword())
+        );
+        verify(userRepository).findById(userId);
+        verify(userRepository).save(user);
+    }
+}
