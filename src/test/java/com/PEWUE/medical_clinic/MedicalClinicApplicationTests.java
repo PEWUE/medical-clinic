2431298efbd4f583cd4986488056c0b67190a562
Paweł Kopeć <pawel.kopec13@gmail.com>
2025-10-21 16:35:55 +0200

    test profile

--- src/test/java/com/PEWUE/medical_clinic/MedicalClinicApplicationTests.java
+++ src/test/java/com/PEWUE/medical_clinic/MedicalClinicApplicationTests.java
@@ -2,8 +2,10 @@ package com.PEWUE.medical_clinic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MedicalClinicApplicationTests {
 
 	@Test
--- src/test/java/com/PEWUE/medical_clinic/controller/AppointmentControllerTest.java
+++ src/test/java/com/PEWUE/medical_clinic/controller/AppointmentControllerTest.java
@@ -11,11 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -30,6 +32,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class AppointmentControllerTest {
     @MockitoBean
     AppointmentService appointmentService;
